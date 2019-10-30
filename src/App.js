7bfcd271ef7aedc55e6ffd74edd656f4a9c2c3d4
pablolucianop <pablolucianop@gmail.com>
2019-10-30 19:08:56 -0300

    compartir

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta es la página de TIBP <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +18,25 @@ function App() {
         >
           Learn React
         </a>
+      <Hola modo={'keke'} estilo='oscuro'/>
       </header>
     </div>
   );
 }
 
+function Hola(props) {
+  return(
+    <div>
+    Hola amigou 
+    {
+      props.modo === 'teens' ? (
+        <p>hola chicos</p>
+      ) : (
+          <p>buenos dias senior</p>
+        )
+    }
+
+    </div>
+  )
+}
 export default App;
